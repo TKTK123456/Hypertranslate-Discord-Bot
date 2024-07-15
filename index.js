bde33ff4afb149dc257affc3514b4862dd028de0
TKTK123456 <103334266+TKTK123456@users.noreply.github.com>
2024-07-15 18:14:05 +0000

    update

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 import hypertranslate from './hypertranslate.js'
 import { Client, GatewayIntentBits } from 'discord.js'
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMembers] });
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+
+client.login(process.env.token)
